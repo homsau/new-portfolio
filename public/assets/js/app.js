6a98d5265af69c91504aebb8e9500f39508057a0
James Cowart <james@Jamess-Macbook.local>
2019-10-10 10:53:34 -0400

    side menu edit

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -82,114 +82,78 @@ $(document).ready(function() {
 //=====================================================//
 //                         VIEWS                       //
 //=====================================================//
-// $(document).on("click", "#portfolio", function() {
-//     var pageHeight = $("body").height();
-//     $(".full-size").height(pageHeight);
-//     console.log(pageHeight);
-//     var carouselHeight = $("#portfolio-item .carousel-img").height();
-//     $(".carousel-controls").height(carouselHeight);
-// });
 $(document).ready(function() {
-    var pageHeight;
-    var carouselHeight;
-    if($(window).width() > 798) {
-        carouselHeight = $("#portfolio-item .carousel-img").height();
-        $(".carousel-controls span").height(400);
-        console.log('carousel height: ' + carouselHeight);
-    }
-    $(window).resize(function() {
-        pageHeight = $("body").height();
-        $(".full-size").height(pageHeight);
-        carouselHeight = $("#portfolio-item .carousel-img").height();
-        $(".carousel-controls span").height(carouselHeight);
-        var testHeight = $('#portfolio-item img').height();
-        console.log('test height: ' + testHeight);
-    }).resize();
+    $(".carousel-indicators > li").click(function() {
+		var slideNum = $(this).attr("data-slide-to");
+		// console.log(slideNum);
+	});
 });
-
 $(document).ready(function() {
+    $(".switcher + #myItems").each(function() {
+        var itemName = document.getElementsByClassName("itemImg");
+        var names = [];
+        for(var i = 0; i < itemName.length; i++) {
+            names[i] = $(itemName[i]).attr("alt");
+            $(".carousel-inner .carousel-item:nth-child(" + (i + 1) + ") a .carousel-caption h3").addClass("" + i + "").text(names[i]);
+        }
+	});
+	$('.carousel-item', '.show-neighbors').each(function(){
+		var next = $(this).next();
+		if (! next.length) {
+		  	next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().addClass("clonedSlide").appendTo($(this));
+	}).each(function(){
+		var prev = $(this).prev();
+		if (! prev.length) {
+		  	prev = $(this).siblings(':last');
+		}
+		prev.children(':nth-last-child(2)').clone().addClass("clonedSlide").prependTo($(this));
+	});
+    
     $(".switcher #grid").click(function() {
-        $(window).resize(function() {
-            $(".portfolio-item").each(function() {
-                $(this).removeClass("carousel-item");
-                $(this).addClass("grid-item");
-                $(".grid").html($(".portfolio-item"));
-            
-                const portfolioRows = $(".portfolio-item");
-                // const portfolioItems = portfolioRows.length;
-                // console.log(portfolioItems);
-                if($(window).width() > 767) {
-                    for(var i = 0; i < portfolioRows.length; i += 3) {
-                        portfolioRows.slice(i, i + 3).wrapAll("<div class='portfolio-row'></div>");
-                        $(this).each(function() {
-                            $(this).addClass("lg-grid-size");
-                            $(this).removeClass("md-grid-size");
-                            $(this).removeClass("mobile-size");
-                        });
-                    }
-                    if (portfolioRows.length % 3 != 0) {
-                        console.log(portfolioRows.length % 3);
-                        $(".grid").append("<div class='portfolio-item'></div>");
-                    }
-                } else if ($(window).width() > 576) {
-                    for(var i = 0; i < portfolioRows.length; i += 2) {
-                        portfolioRows.slice(i, i + 2).wrapAll("<div class='portfolio-row'></div>");
-                        $(this).each(function() {
-                            $(this).addClass("md-grid-size");
-                            $(this).removeClass("lg-grid-size");
-                            $(this).removeClass("mobile-size");
-                        });
-                    }
-                } else {
-                    for(var i = 0; i < portfolioRows.length; i += 1) {
-                        portfolioRows.slice(i, i + 1).wrapAll("<div class='portfolio-row'></div>");
-                        $(this).each(function() {
-                            $(this).addClass("mobile-size");
-                            $(this).removeClass("lg-grid-size");
-                            $(this).removeClass("md-grid-size");
-                        });
-                    }
-                }
-            });
-            $("#myCarousel").removeClass("carousel");
-            $(".carousel-controls").css("display","none");
-            $(".carousel-indicators").css("display","none");
-        }).resize();
+		$(".switcher + #myItems .clonedSlide").hide();
+		$(".switcher + #myItems").removeClass("carousel slide show-neighbors").addClass("grid").attr("data-interval", "false");
+		$(".switcher + #myItems .embed-responsive-item").addClass("grid-item").removeClass("carousel-item");
+		$(".switcher + #myItems .carousel-inner").removeClass("embed-responsive embed-responsive-16by9");
+		$(".carousel-control-prev").css("display","none");
+		$(".carousel-control-next").css("display","none");
+		$(".carousel-indicators").css("display","none");
     });
     $(".switcher #carousel").click(function() {
+		$(".switcher + #myItems .clonedSlide").show();
+		$(".switcher + #myItems").removeClass("grid").addClass("carousel slide show-neighbors").attr("data-interval", "7000");
+		$(".switcher + #myItems .embed-responsive-item").addClass("carousel-item").removeClass("grid-item");
+		$(".switcher + #myItems .carousel-inner").addClass("embed-responsive embed-responsive-16by9");
         $(".portfolio-item").each(function() {
             $(this).removeClass("grid-item");
             $(this).addClass("carousel-item");
             $(".carousel-inner").prepend($(".portfolio-item"));
         });
-        $("#myCarousel").addClass("carousel");
-        $(".carousel-controls").css("display","flex");
-        $(".carousel-indicators").css("display","flex");
+        $("#myItems").addClass("carousel");
+        $(".carousel-control-prev").css("display","flex");
+		$(".carousel-control-next").css("display","flex");
+		$(".carousel-indicators").css("display","inline-flex");
     });
 });
 
 
 /* change carousel item with arrow keys */
 /* close any open modals */
-$(document).keydown(function(e) {
-    if (e.keyCode === 37) {
-       // Previous
-       $(".carousel-control-prev").click();
-       $(".modal").modal("hide");
-       return false;
-    }
-    if (e.keyCode === 39) {
-       // Next
-       $(".carousel-control-next").click();
-       $(".modal").modal("hide");
-       return false;
-    }
-});
-$(document).ready(function() {
-    $('.carousel').carousel( {
-        interval: false
-    });
-});
+// $(document).keydown(function(e) {
+//     if (e.keyCode === 37) {
+//        // Previous
+//        $(".carousel-control-prev").click();
+//        $(".modal").modal("hide");
+//        return false;
+//     }
+//     if (e.keyCode === 39) {
+//        // Next
+//        $(".carousel-control-next").click();
+//        $(".modal").modal("hide");
+//        return false;
+//     }
+// });
 
 //=====================================================//
 //                      RECAPTCHA                      //
