b7d864a342a5b9e45ee760066f796888ac23d4ca
James Cowart <james@Jamess-Macbook.local>
2020-02-11 20:15:09 -0500

    portfolio updates. not complete by a longshot, but damn

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portfolio
+---
+## Description
+*Eat-Da-Burger* is an app with MySQL, Node, Express, Handlebars and a homemade ORM.
+- All of the burgers that have been made are shown in the column on the left
+- The user will input their name before moving on.
+- When a user clicks the "EAT" burger button, the burger is shown on the right along with who ate it
+- This app store all burgers in a database, adds new ones when created, and updates them when eaten
+
+![Gif customer-buy-product](/public/assets/img/demo.gif)
+
+## Demo
+---
+You can view the demo on Heroku [here!](https://gt-eat-da-burger-2018.herokuapp.com/)
---
+++ server.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+var express = require("express");
+var bodyParser = require("body-parser");
+var nodemailer = require('nodemailer');
+var request = require('request')
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+// var siteKey = process.env.SITE_KEY;
+var secretKey = process.env.SECRET_KEY
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/app_controller.js");
+
+app.use("/", routes);
+
+app.post('/submit',function(req,res,e) {
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    console.log("Beep Boop: You are a robot");
+    return;
+  } else {
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    // Hitting GET request to the URL, Google will respond with success or error scenario.
+    request(verificationUrl,function(error,response,body) {
+      body = JSON.parse(body);
+      // Success will be true or false depending upon captcha validation.
+      if(body.success !== undefined && !body.success) {
+        console.log("second nah");
+      } else {
+        var transporter = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: username,
+            pass: password
+          },
+          tls: {
+            rejectUnauthorized: false
+          }
+        });
+        var mailOptions = {
+          from: '"Portfolio Contact 🤘🏻🔥" <james.cowart.portfolio@gmail.com>',
+          to: 'james.p.cowart@gmail.com',
+          subject: 'Contact Me',
+          html: req.body.name + ' is trying to reach you!' + '</br>' +
+            'Name: ' + req.body.name + '</br>' +
+            'Email: ' + req.body.email + '</br>' +
+            'Message: ' + req.body.message
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
+          if(error){
+            console.log(error);
+            console.log('oops');
+          } else {
+            console.log(info.response);
+            console.log('hooray');
+            
+            res.redirect('/');
+          }
+        })
+      }
+    });
+  }
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
