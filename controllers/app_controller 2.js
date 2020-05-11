2c4cbeb8c6ffea15d4822451d16382190fb71b21
James Cowart <james@Jamess-Macbook.local>
2020-05-11 14:30:13 -0400

    contact form test

---
+++ controllers/app_controller 2.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+// =============================================================
+// Routes
+// =============================================================
+require("../public/assets/router/html-routes.js")(router);
+
+module.exports = router;
